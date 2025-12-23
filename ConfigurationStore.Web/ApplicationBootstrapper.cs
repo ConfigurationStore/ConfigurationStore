aa1f7cb4b0b505078c865807df3392414e356f90
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2025-12-23 18:59:50 +0100

    Add `ApplicationBootstrapper` to configure services and dependencies

---
+++ ConfigurationStore.Web/ApplicationBootstrapper.cs
@@ -0,0 +1,44 @@
+﻿using ConfigurationStore.Web.Components.Account;
+using ConfigurationStore.Web.Data;
+
+using LVK.Bootstrapping;
+
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConfigurationStore.Web;
+
+public class ApplicationBootstrapper : IModuleBootstrapper
+{
+    public void Bootstrap(IHostApplicationBuilder builder)
+    {
+        builder.Bootstrap(new Core.ModuleBootstrapper());
+        builder.Services.AddTransient<IHostInitializer<WebApplication>, ApplicationInitializer>();
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+
+        builder.Services.AddCascadingAuthenticationState();
+        builder.Services.AddScoped<IdentityRedirectManager>();
+        builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultScheme = IdentityConstants.ApplicationScheme;
+            options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+        }).AddIdentityCookies();
+
+        string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(connectionString));
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+        builder.Services.AddIdentityCore<ApplicationUser>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = true;
+            options.Stores.SchemaVersion = IdentitySchemaVersions.Version3;
+        }).AddEntityFrameworkStores<ApplicationDbContext>().AddSignInManager().AddDefaultTokenProviders();
+
+        builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+    }
+}
