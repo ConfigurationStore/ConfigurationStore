4352495880700eb96b26ab8b950e5de23237f97a
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2025-12-23 19:00:10 +0100

    Add `ModuleBootstrapper` to configure the `ConfigurationStore.Core` module

---
+++ ConfigurationStore.Core/ModuleBootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using LVK.Bootstrapping;
+
+using Microsoft.Extensions.Hosting;
+
+namespace ConfigurationStore.Core;
+
+public class ModuleBootstrapper : IModuleBootstrapper
+{
+    public void Bootstrap(IHostApplicationBuilder builder)
+    {
+
+    }
+}
