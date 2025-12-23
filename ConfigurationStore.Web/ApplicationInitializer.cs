5059b7c02f353a825f3f16695891a2347ea7205a
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2025-12-23 19:00:01 +0100

    Add `ApplicationInitializer` to configure web application's HTTP pipeline

---
+++ ConfigurationStore.Web/ApplicationInitializer.cs
@@ -0,0 +1,32 @@
+﻿using ConfigurationStore.Web.Components;
+
+using LVK.Bootstrapping;
+
+namespace ConfigurationStore.Web;
+
+public class ApplicationInitializer : IHostInitializer<WebApplication>
+{
+    public Task InitializeAsync(WebApplication host)
+    {
+        // Configure the HTTP request pipeline.
+        if (host.Environment.IsDevelopment())
+        {
+            host.UseMigrationsEndPoint();
+        }
+        else
+        {
+            host.UseExceptionHandler("/Error", createScopeForErrors: true);
+        }
+
+        host.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+        host.UseAntiforgery();
+
+        host.MapStaticAssets();
+        host.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
+        // Add additional endpoints required by the Identity /Account Razor components.
+        host.MapAdditionalIdentityEndpoints();
+
+        return Task.CompletedTask;
+    }
+}
