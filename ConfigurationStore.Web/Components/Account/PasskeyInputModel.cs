36657299fae811e6ec4fd5c80521861142ce104d
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2025-12-23 19:00:21 +0100

    Add `CreateIdentitySchema` migration to initialize ASP.NET Core Identity schema

---
+++ ConfigurationStore.Web/Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,7 @@
+namespace ConfigurationStore.Web.Components.Account;
+
+public class PasskeyInputModel
+{
+    public string? CredentialJson { get; set; }
+    public string? Error { get; set; }
+}
---
+++ ConfigurationStore.Web/Components/Account/PasskeyOperation.cs
@@ -0,0 +1,7 @@
+namespace ConfigurationStore.Web.Components.Account;
+
+public enum PasskeyOperation
+{
+    Create = 0,
+    Request = 1,
+}
